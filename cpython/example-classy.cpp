eff9853ae24349b8665e96fb3796b708d1e63a6f
Hardik Patel <hardikp12@gmail.com>
2017-12-29 09:31:20 -0500

    Minor change

--- cpython/example-classy.cpp
+++ cpython/example-classy.cpp
@@ -2,7 +2,7 @@
 #include <structmember.h>
 
 typedef struct {
-  PyObject_HEAD; // no semicolon
+  PyObject_HEAD // no semicolon
   int number;
 } Classy;
 
@@ -53,43 +53,43 @@ static PyMethodDef Classy_methods[] = {
 };
 
 static PyTypeObject ClassyType = {
-    PyVarObject_HEAD_INIT(NULL, 0) "example.Classy", /* tp_name */
-    sizeof(Classy),                                  /* tp_basicsize */
-    0,                                               /* tp_itemsize */
-    (destructor)Classy_dealloc,                      /* tp_dealloc */
-    0,                                               /* tp_print */
-    0,                                               /* tp_getattr */
-    0,                                               /* tp_setattr */
-    0,                                               /* tp_reserved */
-    0,                                               /* tp_repr */
-    0,                                               /* tp_as_number */
-    0,                                               /* tp_as_sequence */
-    0,                                               /* tp_as_mapping */
-    0,                                               /* tp_hash  */
-    0,                                               /* tp_call */
-    0,                                               /* tp_str */
-    0,                                               /* tp_getattro */
-    0,                                               /* tp_setattro */
-    0,                                               /* tp_as_buffer */
-    Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE,        /* tp_flags */
-    "Classy objects",                                /* tp_doc */
-    0,                                               /* tp_traverse */
-    0,                                               /* tp_clear */
-    0,                                               /* tp_richcompare */
-    0,                                               /* tp_weaklistoffset */
-    0,                                               /* tp_iter */
-    0,                                               /* tp_iternext */
-    Classy_methods,                                  /* tp_methods */
-    Classy_members,                                  /* tp_members */
-    0,                                               /* tp_getset */
-    0,                                               /* tp_base */
-    0,                                               /* tp_dict */
-    0,                                               /* tp_descr_get */
-    0,                                               /* tp_descr_set */
-    0,                                               /* tp_dictoffset */
-    (initproc)Classy_init,                           /* tp_init */
-    0,                                               /* tp_alloc */
-    Classy_new,                                      /* tp_new */
+    PyVarObject_HEAD_INIT(NULL, 0) "example.Classy",  /* tp_name */
+    sizeof(Classy),                           /* tp_basicsize */
+    0,                                        /* tp_itemsize */
+    (destructor)Classy_dealloc,               /* tp_dealloc */
+    0,                                        /* tp_print */
+    0,                                        /* tp_getattr */
+    0,                                        /* tp_setattr */
+    0,                                        /* tp_reserved */
+    0,                                        /* tp_repr */
+    0,                                        /* tp_as_number */
+    0,                                        /* tp_as_sequence */
+    0,                                        /* tp_as_mapping */
+    0,                                        /* tp_hash  */
+    0,                                        /* tp_call */
+    0,                                        /* tp_str */
+    0,                                        /* tp_getattro */
+    0,                                        /* tp_setattro */
+    0,                                        /* tp_as_buffer */
+    Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE, /* tp_flags */
+    "Classy objects",                         /* tp_doc */
+    0,                                        /* tp_traverse */
+    0,                                        /* tp_clear */
+    0,                                        /* tp_richcompare */
+    0,                                        /* tp_weaklistoffset */
+    0,                                        /* tp_iter */
+    0,                                        /* tp_iternext */
+    Classy_methods,                           /* tp_methods */
+    Classy_members,                           /* tp_members */
+    0,                                        /* tp_getset */
+    0,                                        /* tp_base */
+    0,                                        /* tp_dict */
+    0,                                        /* tp_descr_get */
+    0,                                        /* tp_descr_set */
+    0,                                        /* tp_dictoffset */
+    (initproc)Classy_init,                    /* tp_init */
+    0,                                        /* tp_alloc */
+    Classy_new,                               /* tp_new */
 };
 
 static struct PyModuleDef example_definition = {
